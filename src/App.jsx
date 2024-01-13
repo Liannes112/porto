9e3656cdd401f42bedb9688ee5cfcd292028b3c0
Liannes112 <andredn74@gmail.com>
2024-01-13 18:07:00 +0700

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Portofolio from './pages/Portofolio';
 import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <Router basename="/porto">
+    <HashRouter>
       <Routes>
-        <Route path="/porto" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/myporto" element={<Home />} />
         <Route path="/portofolio" element={<Portofolio />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
